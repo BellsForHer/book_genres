b2b2d4e3dca88a96ce8a5c0ec9f8ee641512ff9e
BellsForHer <arhall88@gmail.com>
2023-01-19 20:28:08 -0600

    Create book_genres.rb

---
+++ lib/book_genres.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "book_genres/version"
+require_relative "./cli.rb"
+
+module BookGenres
+  class Error < StandardError; end
+  # Your code goes here...
+end
