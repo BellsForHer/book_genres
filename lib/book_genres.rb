111a393f6598a2d769e1c4947046f0afd2837213
BellsForHer <arhall88@gmail.com>
2023-01-22 16:51:42 -0600

    Update book_genres.rb

--- lib/book_genres.rb
+++ lib/book_genres.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 require_relative "book_genres/version"
-require_relative "./cli.rb"
+require_relative "cli.rb"
+require_relative "genre.rb"
+require_relative "book_info.rb"
+require_relative "scraper.rb"
 
 module BookGenres
   class Error < StandardError; end
