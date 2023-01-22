20af7891d0bcd7036fe25076fe241ca4982c5f16
BellsForHer <arhall88@gmail.com>
2023-01-22 16:51:49 -0600

    Update cli.rb

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,19 +1,44 @@
 class CLI
     def run
-        puts "Welcome!"
-        menu
+        
+        system('clear')
+        greeting
+        while menu != 'exit'
+        end
+        end_program
+    end
+
+    def greeting
+        puts "Welcome to books genre mania!"
+    end
+
+    def end_program
+        puts "Come again soon!"
     end
 
     def menu
-        puts "Puts pick book genre"
-        puts "Travel"
-        puts "Mystery"
-        user input = gets.chomp
-        while user_input != "exit"
-            # puts "Puts pick book genre"
-            # puts "Travel"
-            # puts "Mystery"
-            user_input = gets.chomp
+        #list of options
+        list_options
+        #get user input
+        puts "Pick book genre"
+        input = gets.chomp
+        choose_option(input)
+
+        return input
+        
+    end
+
+    def list_options
+        puts "1. "
+        puts "2. "
+    end
+
+    def choose_option(input)
+        case input 
+        when "1"
+            puts "Number 1 chosen"
+        when "2"
+            puts "Number 2 chosen"
         end
     end
 end
