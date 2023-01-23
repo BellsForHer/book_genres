c044b7f6f685527cf408d920b88553f63a74e146
BellsForHer <arhall88@gmail.com>
2023-01-22 18:49:30 -0600

    Update book_info.rb

--- lib/book_info.rb
+++ lib/book_info.rb
@@ -1,5 +1,19 @@
-class Bookinfo < Genre
 
-    
+class Bookinfo < Genre 
+    attr_accessor :book_title
+    @@books = []
+
+    def initialize(book_list) 
+        @book_title = book_list
+
+        @@books << self
+    end
+
+    #class method
+    def self.all
+        @@books
+    end
 end
+    
+
 
